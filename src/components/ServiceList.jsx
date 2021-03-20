4412b6a2ef401ed81202dc2a9821323d0c19c9f2
Ilya Makarov <feesler@gmail.com>
2021-03-20 16:14:23 +0300

    * Refactor editing item in the service list store - Use single property per list instead of managing flags of each item

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectUpdateService, removeService } from '../actions/actionCreators';
 import { ReactComponent as UpdateIcon } from '../assets/edit.svg';
 import { ReactComponent as DeleteIcon } from '../assets/close.svg';
 
 const ServiceList = () => {
-  const items = useSelector(state => state.serviceList);
+  const list = useSelector(state => state.serviceList);
   const dispatch = useDispatch();
 
   const handleUpdate = (id) => {
-    const item = items.find((service) => service.id === id);
+    const item = list.items.find((service) => service.id === id);
     dispatch(selectUpdateService(item));
   }
 
@@ -19,14 +20,20 @@ const ServiceList = () => {
 
   return (
     <ul className="service-list">
-      {items.map((item) =>
-        <li key={item.id} className="service-item"        >
+      {list.items.map((item) =>
+        <li
+          key={item.id}
+          className={classNames(
+            'service-item',
+            { 'service-item_active': item.id === list.editing }
+          )}
+        >
           <span className="service-item__name">{item.name}</span>
           <span className="service-item__price">{item.price}</span>
-          <button className="icon-btn" disabled={item.disabled} onClick={() => handleUpdate(item.id)}>
+          <button className="icon-btn" onClick={() => handleUpdate(item.id)} >
             <UpdateIcon />
           </button>
-          <button className="icon-btn" disabled={item.disabled} onClick={() => handleDelete(item.id)} >
+          <button className="icon-btn" onClick={() => handleDelete(item.id)} >
             <DeleteIcon />
           </button>
         </li>
--- src/reducers/ServiceList.js
+++ src/reducers/ServiceList.js
@@ -7,19 +7,25 @@ import {
   RESET_SERVICE_FORM,
 } from '../actions/actionTypes';
 
-const initialState = [
-  { id: nanoid(), name: 'Glass replace', price: 15000, disabled: false },
-  { id: nanoid(), name: 'Battery replace', price: 9000, disabled: false },
-];
+const initialState = {
+  items: [
+    { id: nanoid(), name: 'Glass replace', price: 15000 },
+    { id: nanoid(), name: 'Battery replace', price: 9000 },
+  ],
+  editing: null,
+};
 
 export default function serviceListReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_SERVICE: {
       const { name, price } = action.payload;
-      return [
+      return {
         ...state,
-        { id: nanoid(), name, price: Number(price), disabled: false },
-      ];
+        items: [
+          ...state.items,
+          { id: nanoid(), name, price: Number(price) },
+        ]
+      };
     }
 
     case UPDATE_SERVICE: {
@@ -32,19 +38,19 @@ export default function serviceListReducer(state = initialState, action) {
 
     case SELECT_UPDATE_SERVICE: {
       const { id } = action.payload;
-      return state.map((item) => ({
-        ...item,
-        disabled: (item.id === id),
-      }));
+      return { ...state, editing: id };
     }
 
     case REMOVE_SERVICE: {
       const { id } = action.payload;
-      return [...state.filter((item) => item.id !== id)];
+      return {
+        ...state,
+        items: [...state.items.filter((item) => item.id !== id)],
+      };
     }
 
     case RESET_SERVICE_FORM: {
-      return state.map((item) => ({ ...item, disabled: false }));
+      return { ...state, editing: null };
     }
 
     default:
